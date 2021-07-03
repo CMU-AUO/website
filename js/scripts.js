5c66201f80bed17595097896e7103fcf3c78c6ac
Angie Li <angiel@andrew.cmu.edu>
2021-07-03 16:20:02 -0400

    fixed slides not showing up correctly issue

--- js/scripts.js
+++ js/scripts.js
@@ -119,24 +119,23 @@ function dropdownClick() {
     }
   }
 
-  var slideIndex = [1,1,1];
-  /* Class the members of each slideshow group with different CSS classes */
-  var slideId = ["s15_Slides", "f15_Slides", "concerts_Slides"];
-  showSlides(1, 0);
-  showSlides(1, 1);
-  showSlides(1, 2);
-  
-  function plusSlides(n, no) {
+var slideIndex = [1,1];
+/* Class the members of each slideshow group with different CSS classes */
+var slideId = ["slides1", "slides2"];
+showSlides(1, 0);
+showSlides(1, 1);
+
+function plusSlides(n, no) {
     showSlides(slideIndex[no] += n, no);
-  }
-  
-  function showSlides(n, no) {
+}
+
+function showSlides(n, no) {
     var i;
     var x = document.getElementsByClassName(slideId[no]);
-    if (n > x.length) {slideIndex[no] = 1}
-    if (n < 1) {slideIndex[no] = x.length}
+    if (n > x.length) {slideIndex[no] = 1;}
+    if (n < 1) {slideIndex[no] = x.length;}
     for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
+        x[i].style.display = "none";
     }
     x[slideIndex[no]-1].style.display = "block";
-  }
+}
