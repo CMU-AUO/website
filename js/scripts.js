6aca457e6bbe5a868385cdb2ca920446cc01564f
qianxuege <qianxuege15@gmail.com>
2024-07-02 14:19:37 -0400

    added poster image modal, board member image, symphony rehearsal calendar

--- js/scripts.js
+++ js/scripts.js
@@ -123,3 +123,36 @@ function dropdownClick() {
 // window.addEventListener('DOMContentLoaded', () => {
    
 // })
+
+window.addEventListener('DOMContentLoaded', () => {
+    if (window.location.pathname.includes("concerts")) {
+        console.log(window.location.pathname);
+        // Get the modal
+        var modal = document.getElementById("posterModal");
+
+        // Get the image and insert it inside the modal - use its "alt" text as a caption
+        var fallImg = document.getElementById("fallImg");
+        var springImg = document.getElementById("springImg");
+        var modalImg = document.getElementById("modalImg");
+        var captionText = document.getElementById("caption");
+        fallImg.onclick = function(){
+            modal.style.display = "block";
+            modalImg.src = this.src;
+            captionText.innerHTML = this.alt;
+        }
+        springImg.onclick = function(){
+            modal.style.display = "block";
+            modalImg.src = this.src;
+            captionText.innerHTML = this.alt;
+        }
+
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[0];
+
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() {
+            modal.style.display = "none";
+        }
+    }
+    
+})
